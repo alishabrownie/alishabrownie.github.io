3b58fa02860fd7be8ca50f25050c33f8e0ccbe93
alishabrownie <130018293+alishabrownie@users.noreply.github.com>
2024-04-18 16:13:37 +1000

    prettier working

---
+++ week 5/script.js
@@ -0,0 +1,47 @@
+function checkWeather() {
+    console.log("I am just being clicked");
+    let myInput = document.querySelector("#myTemp");
+    let temp = myInput.value;
+    console.log("current temp is", temp);
+    let temp = 17;
+
+if(temp>=20 && temp<30)
+{
+    console.log("it feels sunny and warm") }
+    if (tem)
+else if (temp>=10 && temp<20) {
+  console.log("it feels cold");
+}
+else if (temp >=30) {
+  console.log("it is boiling hot");
+} else if (temp <10) {
+    console.log("it is freezing");
+}
+}
+
+
+
+// let temp = 17;
+// let temp = prompt("what is today's temperature?")
+
+// if(temp>=20 && temp<30)
+// {
+//     console.log("it feels sunny and warm") }
+// else if (temp>=10 && temp<20) {
+//   console.log("it feels cold");
+// }
+// else if (temp >=30) {
+//   console.log("it is boiling hot");
+// } else if (temp <10) {
+//     console.log("it is freezing");
+// }
+
+// let myName = prompt("What is your name?");
+// console.log("Hello", myName)
+// let myFullName = "alisha browning"
+
+// let myName = "alisha";
+// let mySirname = "browning";
+
+// let myFullName = `my full name is ${myName} ${mySirname}`
+// console.log(myFullName);
