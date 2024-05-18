40eef7909a360696dfbc818c1eb75e3d7403bf83
alishabrownie <130018293+alishabrownie@users.noreply.github.com>
2024-05-18 21:43:33 +1000

    a2

--- assignment2/script.js
+++ assignment2/script.js
@@ -21,7 +21,10 @@ function updateProgressBar() {
 }
 // Add other functionalities here
 
-// Mute and unmute button added to allow users to mute and still watch the video if they can't listen to the sound at the time.
+// Mute and unmute button:
+// I added this to allow users to mute the sound and still watch the video if they can't listen to the sound at the time.
+// This means if they find the website while they are in a setting where they can't play the sound out loud, they have easy access turn the sound off.
+// If this was not readily available it could result in the user clicking off the site and not interacting with the artist's music.
 const muteUnmuteButton = document.querySelector("#mute-unmute-button");
 console.log(muteUnmuteButton);
 
@@ -44,7 +47,9 @@ function toggleSound() {
   }
 }
 
-// Full screen button to allow viewers to watch the music video in full view without the distractions of the website/other things on their browser.
+// Full screen button:
+// I added this to allow viewers to watch the music video in full view without the distractions of the website/other things on their browser.
+// It helps to immerse the viewer in the content of the video, providing a more engaging experience.
 video.addEventListener("dblclick", goFullscreen);
 
 const fullscreenButton = document.querySelector("#fullscreen-button");
@@ -60,7 +65,14 @@ function goFullscreen() {
   }
 }
 
-// Fast Forward  https://www.youtube.com/watch?v=gyUx8d49fJQ
+// Fast Forward
+// I added both a forward and rewind button so that the user could easily navigate to different areas of the video.
+// This means if they really like a certain part or miss something and want to rewatch it they easily forward or rewind to that section.
+// At first I used this tutorial for the forward button: https://www.youtube.com/watch?v=gyUx8d49fJQ
+// They used play back rate to make it so when you hold down the mouse the video increases in speed.
+// I ended up going with this method: https://stackoverflow.com/questions/65517594/how-to-implement-fast-forward-functionality-for-an-audio-player-using-javascript
+// This adds seconds to the video's current time every time you click.
+// I like this better because it lets the user navigate the video a bit quicker and also you can still foward/rewind when the video is paused so they don't have to listen to harsh cuts in sound.
 
 const forwardButton = document.querySelector("#forward-button");
 console.log(forwardButton);
@@ -75,7 +87,7 @@ function stopForward() {
   video.playbackRate = 1.0;
 }
 
-//Rewind https://stackoverflow.com/questions/65517594/how-to-implement-fast-forward-functionality-for-an-audio-player-using-javascript
+//Rewind
 const rewindButton = document.querySelector("#rewind-button");
 console.log(rewindButton);
 
@@ -89,7 +101,10 @@ function stopRewind() {
   video.playbackRate = 1.0;
 }
 
-//Replay
+//Replay button:
+//I added this function so that if the user hopefully really loves the music video, they can replay it and watch again without having to rewind all the way to the start.
+//I used this method: https://stackoverflow.com/questions/16646648/html5-reset-video-and-play-again
+// This makes it so the video goes back to the starting time when you click the button.
 const replayButton = document.querySelector("#replay-button");
 console.log(replayButton);
 
